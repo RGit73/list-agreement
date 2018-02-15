86d3c27f4ec696d7069392918d2e4a478b615d36
ldev <rgit73@mail.ru>
2018-02-15 16:23:32 +0400

    start project

---
+++ src/reducers/car.js
@@ -0,0 +1,26 @@
+export default ()=>[
+    {
+      id:1,
+      model:'audi',
+      weight:1.4,
+      desc:'audi car'
+    },
+    {
+      id:2,
+      model:'bwm',
+      weight:1.3,
+      desc:'bwm car'
+    },
+    {
+      id:3,
+      model:'lexus',
+      weight:1.6,
+      desc:'lexus car'
+    },
+    {
+      id:4,
+      model:'lada',
+      weight:1.7,
+      desc:'lada car'
+    }
+  ];
